0be5f29ad87119116fabc77ebddbe285527ba7d2
Cemre Begüm Gök <cemrebgok@gmail.com>
2024-06-09 19:21:56 +0300

    SteamKey'imle TopGames'i dinamik olarak çekecek api yok o yüzden 3. taraf api'si kullanmak zorunda kaldım. SteamSpy kullanarak son 2 hafta içinde en çok oynanan oyunları çektim.

--- SteamTopGames/Controllers/SteamController.cs
+++ SteamTopGames/Controllers/SteamController.cs
@@ -6,48 +6,55 @@ namespace SteamTopGames.Controllers
 {
     public class SteamController : Controller
     {
-        private readonly string apiKey = "3C7FC2BEB7015300718BF2E536C06B16";
-        private readonly HttpClient client = new HttpClient();
+       
+            private readonly HttpClient client = new HttpClient();
 
-        public async Task<IActionResult> Index()
-        {
-            var games = await GetTopGames();
-            return View(games);
-        }
-
-        private async Task<List<SteamGame>> GetTopGames()
-        {
-            var games = new List<SteamGame>();
-            var mostPlayesGamesResponse = await client.GetStringAsync($"http://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?key={apiKey}");
-            var mostPlayedGamesData = JsonSerializer.Deserialize<SteamApiResponse>(mostPlayesGamesResponse);
-
-            foreach (var gameData in mostPlayedGamesData.response.games)
+            public async Task<IActionResult> Index()
             {
-                int appId = gameData.appid;
-                int currentPlayers = gameData.player_count;
-
-                var deailsResponse = await client.GetStringAsync($"http://store.steampowered.com/api/appdetails?appids={appId}");
-                var detailsData = JsonSerializer.Deserialize<Dictionary<string, SteamGameDetails>>(deailsResponse);
-
-                if (detailsData[appId.ToString()].success)
-                {
-                    var gameDetails = detailsData[appId.ToString()].data;
-
-                    var game = new SteamGame()
-                    {
-                        AppId = appId,
-                        Name = gameDetails.name,
-                        CurrentPlayers = currentPlayers,
-                        Description = gameDetails.short_description,
-                        HeaderImage = gameDetails.header_image,
-                    };
-
-                    games.Add(game);
-                }
+                var games = await GetTopGames();
+                return View(games);
             }
 
-            return games.OrderByDescending(g => g.CurrentPlayers).Take(10).ToList();
-            
-        }
+            private async Task<List<SteamGame>> GetTopGames()
+            {
+                var games = new List<SteamGame>();
+
+                // SteamSpy API'den popüler oyunları çekme
+                var steamSpyResponse = await client.GetStringAsync("https://steamspy.com/api.php?request=top100in2weeks");
+                var steamSpyData = JsonSerializer.Deserialize<Dictionary<string, SteamSpyApiResponse>>(steamSpyResponse);
+
+                // En popüler ilk 10 oyunun AppId'lerini alalım
+                var topGameAppIds = steamSpyData.Values.Take(10).Select(game => game.appid).ToList();
+
+                foreach (var appId in topGameAppIds)
+                {
+                    var playersResponse = await client.GetStringAsync($"http://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid={appId}");
+                    var playersData = JsonSerializer.Deserialize<Dictionary<string, Dictionary<string, int>>>(playersResponse);
+                    int currentPlayers = playersData["response"]["player_count"];
+
+                    var detailsResponse = await client.GetStringAsync($"http://store.steampowered.com/api/appdetails?appids={appId}");
+                    var detailsData = JsonSerializer.Deserialize<Dictionary<string, SteamGameDetails>>(detailsResponse);
+
+                    if (detailsData[appId.ToString()].success)
+                    {
+                        var gameDetails = detailsData[appId.ToString()].data;
+
+                        var game = new SteamGame
+                        {
+                            AppId = appId,
+                            Name = gameDetails.name,
+                            CurrentPlayers = currentPlayers,
+                            Description = gameDetails.short_description,
+                            HeaderImage = gameDetails.header_image
+                        };
+
+                        games.Add(game);
+                    }
+                }
+
+                // En çok oynanan ilk 10 oyunu alın
+                return games.OrderByDescending(g => g.CurrentPlayers).Take(10).ToList();
+            }
+        
     }
 }
